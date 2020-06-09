fa4eaa0d4dfd03593e6eab4647742cd575f0fc4f
Arron Lcey <arronslacey@gmail.com>
2020-06-09 23:32:10 +0100

    removed nested project folder

--- PM 272 - Introduction to Health Data Science/exercises/solution_08_01.R
+++
@@ -1,5 +0,0 @@
-# The humvar dataset from above, annotated with VEP 
-# and processed to include some features from VEP that might be helpful in predicting disease
-humvar<-read.csv("humvar.csv") 
-
-# Summarize values in each column
---
+++ binder/install.R
@@ -0,0 +1,4 @@
+install.packages("tidyverse")
+install.packages("rjson")
+install.packages("rpart")
+install.packages("ROCR")
